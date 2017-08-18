b858f471942b5937e2f566c21bd491f8ccdd29b7
ArseneMayam <mayamarsene@yahoo.com>
2017-08-17 20:33:22 -0400

    Travaille sur contact.php : - enregistrement des données dans la BD - Envoie d'accusé de réception au mail du solliciteur

--- contact.php
+++ contact.php
@@ -7,6 +7,8 @@
  */
 
 
+require_once 'data/contactData.php'; // inclue contactData.php
+
 /*var_dump($_SERVER['REQUEST_METHOD']);*/
 $en_post = ('POST' === $_SERVER['REQUEST_METHOD']); // Indique si on est en post ou pas
 
@@ -75,7 +77,7 @@ require_once 'views/top_page.php';
         <h2>Informations</h2>
     </div>
     <div id="div_formContact">
-        <form action="" method="post">
+        <form action="data/contactData.php" method="post">
             <label for="nom">Nom</label>
             <input type="text" name="saisie_nom" id="nom" placeholder="Votre nom"
                    value="<?= $en_post ? $fnom['val'] : '' ?>">
---
+++ data/contactData.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mayammouarangue
+ * Date: 17/08/17
+ * Time: 19:36
+ */
+
+$nom = $_POST['saisie_nom'];
+$prenom = $_POST['saisie_prenom'];
+$email = $_POST['saisie_email'];
+$textMsg = $_POST['message'];
+
+var_dump($nom);
+var_dump($prenom);
+var_dump($email);
+var_dump($textMsg);
+
+// on établie une connexion à la base de donnée
+$liendb = mysqli_connect('localhost', 'root', 'root', 'tp_php');
+mysqli_select_db($liendb, 'tp_php');
+
+// on vérifie si l'on est bien connecté
+if (!$liendb) {
+    die("Connexion échoué!: " . mysqli_connect_error());
+}
+
+
+$sql = "INSERT INTO contact(nom, prenom, email)  
+VALUES('$nom','$prenom','$email ' )";
+
+if (mysqli_query($liendb, $sql)) {
+    echo "Requete reussie";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($liendb);
+}
+
+
+// on envoie un accusé de réception
+// Envoie du formulaire par mail
+
+$destinataire = $email;
+$titre = "Accusé d'envoie";
+
+$message = "";
+$message .= "Bonjour, " . $prenom . ' ' . $nom . "\n";
+$message .= "Ceci est un accusé d'envoie de votre message :" . "\n";
+$message .= $textMsg . "\n";
+$message .= "\n";
+$message .= "\n";
+$message .= "On vous recontacterai très prochainement" . "\n";
+$message .= "- L'Equipe location-Bateaux" . "\n";
+
+
+if (mail($destinataire, $titre, $message)) {
+    echo '<p> Message transmis', '</p>';
+}
+
+
+mysqli_close($liendb);
+
